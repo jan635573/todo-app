4e5cb3f2e8b822b62d58ffe4f9707f5b2dfc8249
UIDAM <jan635573@gmail.com>
2025-01-16 17:56:59 +0900

    insert

---
+++ src/components/TodoTemplate.js
@@ -0,0 +1,11 @@
+
+const TodoTemplate = ({children}) => {
+  return (
+    <div className="todo-template">
+      <h1>일정관리</h1>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+export default TodoTemplate;
