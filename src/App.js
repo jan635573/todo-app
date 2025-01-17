07ff41fce3749c8a8499da18652efbb5909e0ccb
UIDAM <jan635573@gmail.com>
2025-01-17 11:50:24 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import TodoInsert from "./components/TodoInsert";
 import TodoTemplate from "./components/TodoTemplate";
@@ -6,14 +6,25 @@ import TodoList from "./components/TodoList";
 
 const App = () => {
   //id, text, checked
-  const [todos, setTodos] = useState([
-    {id:1, text:"리액트의 기초 알아보기", checked:false},
-    {id:2, text:"컴포넌트 스타일링 해보기", checked:true}
-  ]);
+  const [todos, setTodos] = useState(()=>{
+    //초기값
+    const load = localStorage.getItem('todos');
+    return load ? JSON.parse(load) : [];
+  }
+);
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos));
+  },[todos]);
   const handleInsert = (value) => {
-    const todo = {id: todos.length+1, text:value, checked:false};
+    // const todo = {id: todos.length+1, text:value, checked:false};
+    const todo = {id: Date.now(), text:value, checked:false};
     setTodos([...todos,todo]);
   }
+  // const handleInsert = useCallback((value) => {
+  //   const todo = {id: Date.now(), text:value, checked:false};
+  //   setTodos((prev)=>{return [...prev,todo]});
+  // },[]);
+
   const handleChecked = (id) => {
     const toggle = todos.map((list)=>{
       return (list.id === id) ? {id:list.id, text:list.text, checked:!list.checked} : list;
@@ -25,11 +36,19 @@ const App = () => {
     //   })
     // );
   }
+
+  const handleDelete = (id) => {
+    //id만 제외하고 todo배열을 구성
+    const result = todos.filter((list)=>{
+      return list.id !== id;
+    });
+    setTodos(result);
+  }
   return (
     <div className="app">
       <TodoTemplate>
         <TodoInsert onInsert={handleInsert}/>
-        <TodoList todos={todos} onChecked={handleChecked}/>
+        <TodoList todos={todos} onChecked={handleChecked} onRemove={handleDelete}/>
       </TodoTemplate>
     </div>
   );
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -4,14 +4,17 @@ const TodoInsert = ({onInsert}) => {
   const [value,setValue] = useState("");
   const handleSubmit = (event) => {
     event.preventDefault();
-    onInsert(value);
+    if(value.length > 0){
+      onInsert(value);
+      setValue("");  
+    }
   }
   const handleInput = (event) => {
     setValue(event.target.value);
   }
   return (
     <form className="todo-insert" onSubmit={handleSubmit}>
-      <input type="text" placeholder="할 일을 입력하세요" onChange={handleInput}/>
+      <input type="text" value={value} placeholder="할 일을 입력하세요" onChange={handleInput}/>
       <button type="submit">+</button>
     </form>
   );
